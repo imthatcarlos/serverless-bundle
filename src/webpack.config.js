3c6d34798d93c5a78e1736bbcfcc96390e500894
Carlos Beltran <imthatcarlos@gmail.com>
2019-09-17 16:29:36 -0400

    using babel plugin for modules-commonjs

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -29,7 +29,8 @@ function resolveEntriesPath(entries) {
 function babelLoader() {
   const plugins = [
     "@babel/plugin-transform-runtime",
-    "@babel/plugin-proposal-class-properties"
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-modules-commonjs"
   ];
 
   if (ENABLE_SOURCE_MAPS) {
